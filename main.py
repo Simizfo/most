4951ad6c7da04095efc7927fb6331f7b6dc007ed
Simone Franco <ratchet97@live.it>
2020-06-22 00:30:53 +0200

    first version release

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import sqlite3
 import os
+import requests
+from bs4 import BeautifulSoup
+
+def getURL(page):
+    """
+
+    :param page: html of web page (here: Python home page) 
+    :return: urls in that page 
+    """
+    start_link = page.find("a href")
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1: end_quote]
+    return url, end_quote
+
+if(os.path.exists('data/data.db')):
+    os.remove('data/data.db')
+
+if(os.path.exists('data')):
+    os.rmdir('data')
 
 if not os.path.exists('data'):
     os.mkdir('data')
@@ -8,9 +30,42 @@ sqlconnection = sqlite3.connect('data/data.db')
 
 sql = sqlconnection.cursor()
 
-sql.execute('''CREATE TABLE IF NOT EXISTS urls 
-                (url text, lap int);''')
+sql.execute('''CREATE TABLE IF NOT EXISTS urls (url text, ref text, round int);''')
 
-sql.execute('''INSERT INTO urls VALUES ('www.google.com', 0);''')
+sql.execute('''INSERT INTO urls VALUES ('http://www.google.com', null, 0);''')
 
 sqlconnection.commit()
+
+round = 0
+
+while True:
+    nextround_count = sql.execute('SELECT count(*) FROM urls WHERE round = ?', (round,))
+    print('MOST is ready. Round ' + str(round) + '. I will scan ' + str(nextround_count.fetchone()[0]) + ' urls. Press y to confirm and go.')
+
+    choice = input()
+    if choice == 'y':
+        query = sql.execute('SELECT * FROM urls WHERE round = ?', (round,))
+        results = query.fetchall()
+        print(len(results))
+        for row in results:
+            print(row)
+            try:
+                response = requests.get(row[0])
+            except:
+                print('Maybe not a valid url? check more!')
+            page = str(BeautifulSoup(response.content, features="html.parser"))
+            url = 'starting'
+            while url:
+                url, n = getURL(page)
+                page = page[n:]
+                if url:
+                    if url.startswith('/'):
+                        url = row[0] + url
+                    print(url)
+                    sql.execute('INSERT INTO urls VALUES (?, ?, ?)', (url, row[0], round + 1))
+
+        sqlconnection.commit()
+        round += 1
+    
+    else: 
+        break
